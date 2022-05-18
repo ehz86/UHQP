b86d05b5cba614359cc2041bcb616908077802bc
annafilyakina <anna.filyakina@mail.ru>
2022-05-18 10:29:52 +0300

    ДОБАВИЛИ КНОПОЧКИ-КОНТРОЛИЧКИ)))))

--- UHQP/src/main/java/com/example/uhqp/HelloController.java
+++ UHQP/src/main/java/com/example/uhqp/HelloController.java
@@ -1,14 +1,40 @@
 package com.example.uhqp;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+    @FXML
+    private Button HomeButton;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    void HomeButtonPress(ActionEvent event) {
     }
+    @FXML
+    void BackButtonPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ForwardButtonPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void PlayButtonPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void PressPlaylistButton(ActionEvent event) {
+
+    }
+
+
 }
