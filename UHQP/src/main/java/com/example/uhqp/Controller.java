9b2b5f8073de0dacfb98107bff82afd1fc00f10f
Egor <91316084+ehz86@users.noreply.github.com>
2022-06-08 07:59:45 +0300

    Update Controller.java

--- UHQP/src/main/java/com/example/uhqp/Controller.java
+++ UHQP/src/main/java/com/example/uhqp/Controller.java
@@ -123,29 +123,20 @@ public class Controller {
 
     }
 
-    @FXML
+     @FXML
+
     void PlayButtonPressed(ActionEvent event) {
-        /*Image imagePlay = new Image(new File("@images/play.png").toString());
-        ivPlay = new ImageView(imagePlay);
-        Image imagePause = new Image(new File("@images/pause.png").toString());
-        ivPause = new ImageView(imagePlay);
-        */
-
-
         MediaPlayer.Status status = mediaPlayer.getStatus();
 
-
-        if (status == MediaPlayer.Status.PLAYING) {
+        if(status == MediaPlayer.Status.PLAYING) {
             mediaPlayer.pause();
-            //playButton.setGraphic(ivPlay);
-        } else if (status == MediaPlayer.Status.PAUSED || status == MediaPlayer.Status.STOPPED) {
-            mediaPlayer.play();
-            //playButton.setGraphic(ivPause);//апдейт имэдж вью и релоуднуть сцену
+            playImg.setImage(imagePlay);
         }
-
-
+        else {
+            mediaPlayer.play();
+            playImg.setImage(imagePause);//апдейт имэдж вью и релоуднуть сцену
+        }
     }
-
     @FXML
     void PressPlaylistButton(ActionEvent event) {
     }
