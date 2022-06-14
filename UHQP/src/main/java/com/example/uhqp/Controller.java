2bd627d11d479ec8e34de9b710ce9e0eb2a0cc53
Egor <hohloff.jegor@yandex.ru>
2022-06-14 20:31:53 +0300

    Add forward button(not working correctly)

--- UHQP/src/main/java/com/example/uhqp/Controller.java
+++ UHQP/src/main/java/com/example/uhqp/Controller.java
@@ -1,5 +1,5 @@
 package com.example.uhqp;
-
+import java.util.Collections;
 import javafx.beans.InvalidationListener;
 import javafx.beans.Observable;
 import javafx.beans.binding.Bindings;
@@ -22,12 +22,16 @@ import javafx.util.Duration;
 
 import java.io.File;
 import java.net.MalformedURLException;
+import java.util.Collection;
+
 import java.util.List;
 
 public class Controller {
 
     private String path;
-    private File cs2;
+    private List<File> songs;
+    private File cs,cs2;
+    private int index;
     private MediaPlayer mediaPlayer;
     @FXML
     MediaView mediaView;
@@ -53,9 +57,9 @@ public class Controller {
 
 
     @FXML
-    void HomeButtonPress(ActionEvent event) throws MalformedURLException {
-
-
+    void HomeButtonPress(ActionEvent event) {
+        System.out.println(songs);
+        Collections.shuffle(songs);
 
     }
 
@@ -68,14 +72,70 @@ public class Controller {
 
     @FXML
     void ForwardButtonPressed(ActionEvent event) {
+        if(index<=songs.size()-1){
+            MediaPlayer.Status status = mediaPlayer.getStatus() ;
+            index++;
+            mediaPlayer.pause();
+            cs = ListView.getItems().get(index);
+            mediaPlayer.play();
+            if(cs2 != cs) {
+                mediaPlayer.pause();
+                status = MediaPlayer.Status.READY;
+            }
+            if (status == MediaPlayer.Status.READY) {
+                path = cs.toURI().toString();
+                if (path != null) {
+                    media = new Media(path);
+                    mediaPlayer = new MediaPlayer(media);
+                    DoubleProperty width = mediaView.fitWidthProperty();
+                    DoubleProperty height = mediaView.fitWidthProperty();
 
+                    width.bind(Bindings.selectDouble(mediaView.sceneProperty(), "width"));
+                    height.bind(Bindings.selectDouble(mediaView.sceneProperty(), "height"));
+                    mediaPlayer.currentTimeProperty().addListener(new ChangeListener<Duration>() {
+                        @Override
+                        public void changed(ObservableValue<? extends Duration> observableValue, Duration oldValue, Duration newValue) {
+                            progressBar.setValue(newValue.toSeconds());
+                        }
+                    });
+                    progressBar.setOnMousePressed(new EventHandler<MouseEvent>() {
+                        @Override
+                        public void handle(MouseEvent mouseEvent) {
+                            mediaPlayer.seek(Duration.seconds(progressBar.getValue()));
+                        }
+                    });
+                    progressBar.setOnMouseDragged(new EventHandler<MouseEvent>() {
+                        @Override
+                        public void handle(MouseEvent mouseEvent) {
+                            mediaPlayer.seek(Duration.seconds(progressBar.getValue()));
+                        }
+                    });
+
+                    volumeBar.setValue(mediaPlayer.getVolume() * 10);
+                    volumeBar.valueProperty().addListener(new InvalidationListener() {
+                        @Override
+                        public void invalidated(Observable observable) {
+                            mediaPlayer.setVolume(volumeBar.getValue() / 100);
+                        }
+                    });
+                    mediaPlayer.play();
+                }
+            }
+
+        }
+        else{
+            index = 0;
+            cs = ListView.getItems().get(index);
+        }
     }
 
      @FXML
 
     void PlayButtonPressed(ActionEvent event) {
+        System.out.println(event);
         MediaPlayer.Status status = mediaPlayer.getStatus() ;
-        File cs = ListView.getSelectionModel().getSelectedItem();
+        index = ListView.getSelectionModel().getSelectedIndex();
+        cs = ListView.getItems().get(index);
         if(cs2 != cs) {
             mediaPlayer.pause();
             status = MediaPlayer.Status.READY;
@@ -137,7 +197,7 @@ public class Controller {
     @FXML
     void PressPlaylistButton(ActionEvent event) {
         FileChooser fileChooser = new FileChooser();
-        List<File> songs = fileChooser.showOpenMultipleDialog(null);
+        songs = fileChooser.showOpenMultipleDialog(null);
 
         ListView.getItems().addAll(songs);
         media = new Media(songs.get(0).toURI().toString());
