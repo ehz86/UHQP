41e5502592a25481d56722931ba1cb6e94f28075
Egor <hohloff.jegor@yandex.ru>
2022-06-14 23:51:09 +0300

    Add forward button(not working correctly)

--- UHQP/src/main/java/com/example/uhqp/Controller.java
+++ UHQP/src/main/java/com/example/uhqp/Controller.java
@@ -1,5 +1,31 @@
 package com.example.uhqp;
-
+/* Copyright (C) 2022 <Poopy studio>
+   This file is part of the UHQP.
+   (Yet Another Merge Sort Routines)
+    UHQP is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+   UHQP is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with UHQP.  If not, see <http://www.gnu.org/licenses/>.
+  (Этот файл — часть UHQP.
+   UHQP - свободная программа: вы можете перераспространять ее и/или
+   изменять ее на условиях Стандартной общественной лицензии GNU в том виде,
+   в каком она была опубликована Фондом свободного программного обеспечения;
+   либо версии 3 лицензии, либо (по вашему выбору) любой более поздней
+   версии.
+   UHQP распространяется в надежде, что она будет полезной,
+   но БЕЗО ВСЯКИХ ГАРАНТИЙ; даже без неявной гарантии ТОВАРНОГО ВИДА
+   или ПРИГОДНОСТИ ДЛЯ ОПРЕДЕЛЕННЫХ ЦЕЛЕЙ. Подробнее см. в Стандартной
+   общественной лицензии GNU.
+   Вы должны были получить копию Стандартной общественной лицензии GNU
+   вместе с этой программой. Если это не так, см.
+   <http://www.gnu.org/licenses/>.)
+ */
 import java.util.Collections;
 
 import javafx.beans.InvalidationListener;
@@ -27,6 +53,7 @@ import java.net.MalformedURLException;
 import java.util.Collection;
 
 import java.util.List;
+import java.util.Timer;
 
 public class Controller {
 
@@ -137,6 +164,8 @@ public class Controller {
                 }
             });
 
+         
+
             volumeBar.setValue(mediaPlayer.getVolume() * 10);
             volumeBar.valueProperty().addListener(new InvalidationListener() {
                 @Override
@@ -145,6 +174,7 @@ public class Controller {
                 }
             });
             mediaPlayer.play();
+
             gif2004.setImage(gifdance);
         }
     }
